042ddab2e1c2d8cd26d0db6b0e7c8c8069db434a
zilohumberto <zilohumberto@gmail.com>
2020-04-27 23:41:14 -0300

    #3

--- apps/sizes/views.py
+++ apps/sizes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from sizes.models import Size
+from sizes.serializers import SizeSerializer
 
-# Create your views here.
+
+class SizeView(ModelViewSet):
+    serializer_class = SizeSerializer
+    queryset = Size.objects.all()
+    # permission_classes = (IsAuthenticated,)
+    http_method_names = ('get', 'patch', 'post', 'delete', )
